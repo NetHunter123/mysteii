f1e408ca9804f2f4c6b70c37dacb74fedc451a07
NetHunter123 <viktor3@i.ua>
2024-06-03 16:50:27 +0300

    upd head title

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
 	return (
 		<>
 			<Head>
-				<title>Экстрасенс</title>
+				<title>Анна Астахова</title>
 				<meta name="description" content="Помогу с любым вопросом"/>
 				<meta name="viewport" content="width=device-width, initial-scale=1"/>
 				<link rel="icon" href="/favicon.ico"/>
